59264b9d1ed5685354f3794db1d50b5d0effe5a8
Jimmy Wira Arbaa <jimmywiraarbaa03@gmail.com>
2025-09-03 14:31:54 +0700

    tambah middleware untuk get data antrian

--- routes/antrianRoutes.js
+++ routes/antrianRoutes.js
@@ -6,9 +6,11 @@ const {
   // updateAntrian,
 } = require('../controllers/antrianControllers');
 
+const authMiddleware = require('../middleware/authMiddleware');
+
 // endpoint
 router.post('/', createAntrian);        // POST /api/antrian
-router.get('/', getAntrian);        // GET /api/antrian
+router.get('/', authMiddleware, getAntrian);        // GET /api/antrian
 // router.put('/:id', updateAntrian);     // PUT /api/antrian/:id
 
 module.exports = router;
